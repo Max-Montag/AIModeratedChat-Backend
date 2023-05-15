6947214553d14f7649616b546f656c368fce28b8
MaMon94 <max.montag1994@gmail.com>
2023-05-15 17:27:49 +0200

    minor

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import MessageListCreateView
+from .views import MessageListCreateView, ChatRoomCreateView
 
 urlpatterns = [
-    path('api/chatrooms/<int:chatroom_id>/messages/', MessageListCreateView.as_view()),
+    path('api/chatrooms/<int:chatroom_id>/messages/',
+         MessageListCreateView.as_view()),
+    path('api/chatrooms/create', ChatRoomCreateView.as_view()),
 ]
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,13 +1,22 @@
-from rest_framework import generics, ListCreateAPIView
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
 from .models import ChatRoom, Message
 from .serializers import ChatRoomSerializer, MessageSerializer
+
 
 class MessageListCreateView(generics.ListCreateAPIView):
     serializer_class = MessageSerializer
 
     def get_queryset(self):
         return Message.objects.filter(chatroom_id=self.kwargs['chatroom_id'])
-    
+
+
 class ChatRoomCreateView(ListCreateAPIView):
     queryset = ChatRoom.objects.all()
     serializer_class = ChatRoomSerializer
+
+    def post(self, request, *args, **kwargs):
+        chatroom = ChatRoom.objects.create()
+        serializer = self.get_serializer(chatroom)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
