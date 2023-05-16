2890d6b5e772a07bb7c6b6d9d2208414ac9e148a
MaMon94 <max.montag1994@gmail.com>
2023-05-16 11:37:44 +0200

    login unfinished

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import MessageListCreateView
+from .views import MessageListCreateView, RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('chatrooms/<str:chatroom_id>/messages/',
          MessageListCreateView.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,5 +1,8 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Message
 from .serializers import MessageSerializer
 
@@ -11,9 +14,26 @@ class MessageListCreateView(generics.ListCreateAPIView):
         return Message.objects.filter(chatroom_id=self.kwargs['chatroom_id'])
 
     def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
         if self.request.user.is_authenticated:
             author = self.request.user
         else:
             # TODO - remove this default author
             author, _ = User.objects.get_or_create(username='default')
         serializer.save(author=author)
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            return Response(
+                {"error": "Username and password required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user = User.objects.create_user(username=username, password=password)
+
+        return Response({"message": "User created"}, status=status.HTTP_201_CREATED)
